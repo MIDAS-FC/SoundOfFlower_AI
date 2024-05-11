4572c4623ef3107902bea0de1c8338faf48fda0e
dongyun <ehddbs452100@naver.com>
2024-05-11 14:01:33 +0900

    Load Kobert Model From Local

--- load_kobert.py
+++ load_kobert.py
@@ -16,10 +16,14 @@ print(f"Using device: {device}")
 
 s3 = boto3.client('s3')
 
-bucket_name = 'soundofflower'  
-object_name = 'kobert_emotion_model.pt'  
-response = s3.get_object(Bucket=bucket_name, Key=object_name)
-model_state_dict = torch.load(BytesIO(response['Body'].read()), map_location=device)
+
+model_file_path = 'kobert_emotion_model.pt'  
+model_state_dict = torch.load(model_file_path, map_location=device)
+
+# bucket_name = 'soundofflower'  
+# object_name = 'kobert_emotion_model.pt'  
+# response = s3.get_object(Bucket=bucket_name, Key=object_name)
+# model_state_dict = torch.load(BytesIO(response['Body'].read()), map_location=device)
 
 tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
 
